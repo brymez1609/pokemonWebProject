b33beb6c45cb9f35fb145e7578f46be87a716793
Bryan Gomez <bryan12gh@gmail.com>
2021-10-03 23:01:17 -0500

    Create APIVIew methods for CRUD pokemon

--- pokemonProject/utils.py
+++ pokemonProject/utils.py
@@ -1,53 +1,79 @@
 import pandas
+from math import isnan
+
+from django.http import Http404
 
 
 class CsvTool:
     def __init__(self, file_dir_name):
         self.file_dir_name = file_dir_name
-        self.pokemon_data = None
+        self.pokemon_data_frame = None
         self.read_file()
-        self.head_names_list = self.pokemon_data.head().columns.tolist()
+        self.head_names_list = self.pokemon_data_frame.head().columns.tolist()
 
     def get(self, page=0, page_size=10, index_id=None):
         pokemon_dict = []
         if index_id is None:
-            self.head_names_list.insert(0, "index_id")
-            filtered_pokemon_list = self.pokemon_data.to_records()[
+            if not "index_id" in self.head_names_list:
+                self.head_names_list.insert(0, "index_id")
+            self.pokemon_data_frame = self.pokemon_data_frame.fillna(" ")
+            filtered_pokemon_list = self.pokemon_data_frame.to_records()[
                                     page_size * page:(page_size * page) + page_size].tolist()
             for pokemon in filtered_pokemon_list:
-                pokemon_dict.append(dict(zip(self.head_names_list, pokemon)))
-        elif index_id - 1 >= 0:
-            filtered_pokemon_list = self.pokemon_data.loc[index_id, :].to_list()
-            pokemon_dict.append(dict(zip(self.head_names_list, filtered_pokemon_list)))
+                pokemon_clean_nan = self.clean_NaN_values(pokemon)
+                pokemon_dict.append(dict(zip(self.head_names_list, pokemon_clean_nan)))
+        else:
+            try:
+                filtered_pokemon_list = self.pokemon_data_frame.loc[index_id, :].to_list()
+            except KeyError:
+                raise Http404
+            pokemon_clean_nan = self.clean_NaN_values(filtered_pokemon_list)
+            pokemon_dict.append(dict(zip(self.head_names_list, pokemon_clean_nan)))
+            if len(pokemon_dict) == 0:
+                raise Http404
         return pokemon_dict
 
     def read_file(self):
-        self.pokemon_data = pandas.read_csv(self.file_dir_name)
+        self.pokemon_data_frame = pandas.read_csv(self.file_dir_name)
 
     def edit(self, index_id, json=None):
+        pokemon_dict = []
+        try:
+            self.pokemon_data_frame.loc[index_id, :].to_list()
+        except KeyError:
+            raise Http404
         for field in json.keys():
-            self.pokemon_data.loc[index_id, field] = json[field]
-        self.pokemon_data.to_csv(self.file_dir_name, index=False)
+            self.pokemon_data_frame.loc[index_id, field] = json[field]
+        pokemon_clean_nan = self.clean_NaN_values(self.pokemon_data_frame.loc[index_id, :].to_list())
+        pokemon_dict.append(dict(zip(self.head_names_list, pokemon_clean_nan)))
+        self.pokemon_data_frame.to_csv(self.file_dir_name, index=False)
+
+        json["index_id"] = self.pokemon_data_frame.index.shape[0]
+        return pokemon_dict
 
     def create(self, json=None):
-        pokemon_number = int(self.pokemon_data.to_records()[-1:]["#"]) + 1
-        json = {'#': pokemon_number, 'Name': 'La Bulba ', 'Type 1': 'Grass', 'Type 2': 'Poison', 'Total': 318, 'HP': 45,
-                     'Attack': 49, 'Defense': 49, 'Sp. Atk': 65, 'Sp. Def': 65, 'Speed': 45, 'Generation': 1,
-                     'Legendary': False}
-        self.pokemon_data = self.pokemon_data.append(json, ignore_index=True)
-        self.pokemon_data.to_csv(self.file_dir_name, index=False)
+        pokemon_number = int(self.pokemon_data_frame.to_records()[-1:]["#"]) + 1
+        json["#"] = pokemon_number
+        json["index_id"] = self.pokemon_data_frame.index.shape[0]
+        self.pokemon_data_frame = self.pokemon_data_frame.append(json, ignore_index=True)
+
+        self.pokemon_data_frame.to_csv(self.file_dir_name, index=False)
+        return json
 
     def delete(self, index_id):
-        self.pokemon_data.drop(self.pokemon_data.index[index_id],
-                                                   axis=0, inplace=True)
-        self.pokemon_data.to_csv(self.file_dir_name, index=False)
+        self.pokemon_data_frame.drop(self.pokemon_data_frame.index[index_id],
+                               axis=0, inplace=True)
+        self.pokemon_data_frame.to_csv(self.file_dir_name, index=False)
 
+    def clean_NaN_values(self, list_values):
+        new_list = []
+        for value in list_values:
+            if isinstance(value, float):
+                if isnan(value):
+                    new_list.append("")
+                else:
+                    new_list.append(value)
+            else:
+                new_list.append(value)
+        return new_list
 
-if __name__ == "__main__":
-    csvTool = CsvTool("../pokemon.csv")
-    #csvTool.delete(index_id=0)
-    #csvTool.create()
-    data = csvTool.get(page=0, page_size=3)
-    #data = csvTool.get(index_id=7)
-    for a in data:
-        print(a)
